34cdfd06d41a9a919402e1fa7c9ad9ea138d0c6b
Lightingbolt2469 <vihaancool23@gmail.com>
2026-08-19 17:36:54 -0400

    Initial commit for Astroic Adventure

---
+++ main.cpp
@@ -0,0 +1,250 @@
+#include <raylib.h>
+
+// Checks if the Asteroid touches the Grenade
+void Collide_Grenade_Asteroid(int &a_x, int &b_x, int &a_y, int &b_y, int a_width, int b_width, int a_height, int b_height, int &a_speed, int &b_speed, bool &b_active, Sound ExplodeSound, int &Score)
+{
+    if (a_x < b_x + b_width && b_x < a_x + a_width && a_y < b_y + b_height && b_y < a_y + a_height)
+    {
+        a_y = 0;
+        a_speed = GetRandomValue(2, 5);
+        a_x = GetRandomValue(0, GetScreenWidth() - a_width);
+        b_active = false;
+        PlaySound(ExplodeSound);
+        Score += 1;
+    }
+}
+
+// Checks if the Player Hits the
+void Player_Hit_Asteroid(int &a_x, int &b_x, int &a_y, int &b_y, int a_width, int b_width, int a_height, int b_height, int a_active, int &Lives)
+{
+    if (a_x < b_x + b_width && b_x < a_x + a_width && a_y < b_y + b_height && b_y < a_y + a_height)
+    {
+        a_x = GetRandomValue(0, GetScreenWidth() - a_width);
+        a_y = 0;
+        Lives -= 1;
+    }
+}
+
+int main()
+{
+    // Starts the game window and sets the fps of the game
+    InitWindow(1200, 800, "Space Shooters");
+    InitAudioDevice();
+    SetTargetFPS(60);
+    // These are all the game variables required to run the game
+    int GameActive = 1;
+    int PlayerWidth = 124;
+    int PlayerHeight = 124;
+    int PlayerX = 600 - (PlayerWidth / 2);
+    int PlayerY = 720 - (PlayerHeight / 2);
+    int PlayerSpeed = 7;
+    int AsteroidWidth = 176;
+    int AsteroidHeight = 176;
+    int AsteroidX = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+    int Asteroid2X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+    int Asteroid3X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+    int Asteroid4X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+    int Asteroid5X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+    int AsteroidY = 0;
+    int Asteroid2Y = 0;
+    int Asteroid3Y = 0;
+    int Asteroid4Y = 0;
+    int Asteroid5Y = 0;
+    int AsteroidSpeed = GetRandomValue(2, 5);
+    int Asteroid2Speed = GetRandomValue(2, 5);
+    int Asteroid3Speed = GetRandomValue(2, 5);
+    int Asteroid4Speed = GetRandomValue(2, 5);
+    int Asteroid5Speed = GetRandomValue(2, 5);
+    bool DrawAsteroid = true;
+    int GrenadeWidth = 30;
+    int GrenadeHeight = 30;
+    int GrenadeX = PlayerX + (GrenadeWidth + PlayerSpeed);
+    int GrenadeY = PlayerY + (PlayerHeight / 2);
+    int OldGrenadeX;
+    int GrenadeSpeed = 15;
+    bool GrenadeActive = false;
+    bool MeasuringOldX = true;
+    Texture2D PlayerSprite = LoadTexture("Sprites/Player.png");
+    Texture2D GrenadeTexture = LoadTexture("Sprites/Grenade.png");
+    Texture2D AsteroidSprite = LoadTexture("Sprites/Asteroid.png");
+    Texture2D Background = LoadTexture("Sprites/Background.png");
+    Sound ExplodeSound = LoadSound("Sounds/Explosion.wav");
+    Sound GrenadeShooting = LoadSound("Sounds/GrenadeShooting.wav");
+    int Score = 1;
+    int Lives = 5;
+    // Game loop
+    while (!WindowShouldClose())
+    {
+        if (GameActive == 1)
+        {
+            // Player Movement
+            if (IsKeyDown(KEY_RIGHT))
+            {
+                PlayerX += PlayerSpeed;
+            }
+            if (IsKeyDown(KEY_LEFT))
+            {
+                PlayerX -= PlayerSpeed;
+            }
+            if (IsKeyDown(KEY_D))
+            {
+                PlayerX += PlayerSpeed;
+            }
+            if (IsKeyDown(KEY_A))
+            {
+                PlayerX -= PlayerSpeed;
+            }
+            // This is where everything gets drawn
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawTexture(Background, 0, 0, WHITE);
+            DrawTexture(GrenadeTexture, OldGrenadeX, GrenadeY, WHITE);
+            DrawTexture(PlayerSprite, PlayerX, PlayerY, WHITE);
+            GrenadeX = PlayerX + (GrenadeWidth + PlayerSpeed);
+            if (DrawAsteroid == true)
+            {
+                DrawTexture(AsteroidSprite, AsteroidX, AsteroidY, WHITE);
+                DrawTexture(AsteroidSprite, Asteroid2X, Asteroid2Y, WHITE);
+                DrawTexture(AsteroidSprite, Asteroid3X, Asteroid3Y, WHITE);
+                DrawTexture(AsteroidSprite, Asteroid4X, Asteroid4Y, WHITE);
+                DrawTexture(AsteroidSprite, Asteroid5X, Asteroid5Y, WHITE);
+            }
+            // Asteroid Movement
+            AsteroidY += AsteroidSpeed;
+            Asteroid2Y += Asteroid2Speed;
+            Asteroid3Y += Asteroid3Speed;
+            Asteroid4Y += Asteroid4Speed;
+            Asteroid5Y += Asteroid5Speed;
+            if (PlayerX <= 0)
+            {
+                PlayerX = 0;
+            }
+            if (PlayerX + PlayerWidth >= GetScreenWidth())
+            {
+                PlayerX = GetScreenWidth() - PlayerWidth;
+            }
+            // This trigers the grenade
+            if (IsKeyDown(KEY_SPACE))
+            {
+                GrenadeActive = true;
+                PlaySound(GrenadeShooting);
+            }
+            Collide_Grenade_Asteroid(AsteroidX, GrenadeX, AsteroidY, GrenadeY, AsteroidWidth, GrenadeWidth, AsteroidHeight, GrenadeHeight, AsteroidSpeed, GrenadeSpeed, GrenadeActive, ExplodeSound, Score);
+            Collide_Grenade_Asteroid(Asteroid2X, GrenadeX, Asteroid2Y, GrenadeY, AsteroidWidth, GrenadeWidth, AsteroidHeight, GrenadeHeight, Asteroid2Speed, GrenadeSpeed, GrenadeActive, ExplodeSound, Score);
+            Collide_Grenade_Asteroid(Asteroid3X, GrenadeX, Asteroid3Y, GrenadeY, AsteroidWidth, GrenadeWidth, AsteroidHeight, GrenadeHeight, Asteroid3Speed, GrenadeSpeed, GrenadeActive, ExplodeSound, Score);
+            Collide_Grenade_Asteroid(Asteroid4X, GrenadeX, Asteroid4Y, GrenadeY, AsteroidWidth, GrenadeWidth, AsteroidHeight, GrenadeHeight, Asteroid4Speed, GrenadeSpeed, GrenadeActive, ExplodeSound, Score);
+            Collide_Grenade_Asteroid(Asteroid5X, GrenadeX, Asteroid5Y, GrenadeY, AsteroidWidth, GrenadeWidth, AsteroidHeight, GrenadeHeight, Asteroid5Speed, GrenadeSpeed, GrenadeActive, ExplodeSound, Score);
+            // Grenade Movement
+            if (GrenadeActive)
+            {
+                if (GrenadeY > 0)
+                {
+                    GrenadeY -= GrenadeSpeed;
+                    MeasuringOldX = false;
+                }
+                else
+                {
+                    GrenadeActive = false;
+                }
+            }
+            if (MeasuringOldX)
+            {
+                OldGrenadeX = GrenadeX;
+            }
+            if (!GrenadeActive)
+            {
+                GrenadeY = PlayerY + (PlayerHeight / 2);
+                MeasuringOldX = true;
+            }
+            if (MeasuringOldX)
+            {
+                OldGrenadeX = GrenadeX;
+            }
+            // This is where the asteroids get reset
+            if (AsteroidY + AsteroidHeight >= GetScreenHeight())
+            {
+                DrawAsteroid = false;
+                AsteroidY = 0;
+                AsteroidX = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+                DrawAsteroid = true;
+                Score -= 1;
+            }
+            if (Asteroid2Y + AsteroidHeight >= GetScreenHeight())
+            {
+                DrawAsteroid = false;
+                Asteroid2Y = 0;
+                Asteroid2X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+                DrawAsteroid = true;
+                Score -= 1;
+            }
+            if (Asteroid3Y + AsteroidHeight >= GetScreenHeight())
+            {
+                DrawAsteroid = false;
+                Asteroid3Y = 0;
+                Asteroid3X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+                DrawAsteroid = true;
+                Score -= 1;
+            }
+            if (Asteroid4Y + AsteroidHeight >= GetScreenHeight())
+            {
+                DrawAsteroid = false;
+                Asteroid4Y = 0;
+                Asteroid4X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+                DrawAsteroid = true;
+                Score -= 1;
+            }
+            if (Asteroid5Y + AsteroidHeight >= GetScreenHeight())
+            {
+                DrawAsteroid = false;
+                Asteroid5Y = 0;
+                Asteroid5X = GetRandomValue(0, GetScreenWidth() - AsteroidWidth);
+                DrawAsteroid = true;
+                Score -= 1;
+            }
+            Player_Hit_Asteroid(AsteroidX, PlayerX, AsteroidY, PlayerY, AsteroidWidth, PlayerWidth, AsteroidHeight, PlayerHeight, DrawAsteroid, Lives);
+            Player_Hit_Asteroid(Asteroid2X, PlayerX, Asteroid2Y, PlayerY, AsteroidWidth, PlayerWidth, AsteroidHeight, PlayerHeight, DrawAsteroid, Lives);
+            Player_Hit_Asteroid(Asteroid3X, PlayerX, Asteroid3Y, PlayerY, AsteroidWidth, PlayerWidth, AsteroidHeight, PlayerHeight, DrawAsteroid, Lives);
+            Player_Hit_Asteroid(Asteroid4X, PlayerX, Asteroid4Y, PlayerY, AsteroidWidth, PlayerWidth, AsteroidHeight, PlayerHeight, DrawAsteroid, Lives);
+            Player_Hit_Asteroid(Asteroid5X, PlayerX, Asteroid5Y, PlayerY, AsteroidWidth, PlayerWidth, AsteroidHeight, PlayerHeight, DrawAsteroid, Lives);
+            // Draws the score
+            DrawText(TextFormat("Score : %i", Score), 75, 50, 60, WHITE);
+            DrawText(TextFormat("Lives : %i", Lives), 75, 110, 60, WHITE);
+            if (Score == 0 || Lives == 0)
+            {
+                GameActive = 0;
+            }
+            if (Score == 100)
+            {
+                GameActive = 2;
+            }
+            EndDrawing();
+        }
+        if (GameActive == 0)
+        {
+            // You Died message
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("You Died", GetScreenWidth() / 2 - MeasureText("You Died", 60) / 2, GetScreenHeight() / 2 - 30, 60, WHITE);
+            EndDrawing();
+        }
+        if (GameActive == 2)
+        {
+            // You Won message
+            BeginDrawing();
+            ClearBackground(BLACK);
+            DrawText("You Won", GetScreenWidth() / 2 - MeasureText("You Won", 60) / 2, GetScreenHeight() / 2 - 30, 60, WHITE);
+            EndDrawing();
+        }
+    }
+    // Every texture and sound gets unloaded here
+    UnloadTexture(PlayerSprite);
+    UnloadTexture(GrenadeTexture);
+    UnloadTexture(AsteroidSprite);
+    UnloadTexture(Background);
+    UnloadSound(ExplodeSound);
+    UnloadSound(GrenadeShooting);
+    CloseAudioDevice();
+    CloseWindow();
+    // This returns 0 meaning the script was a success
+    return 0;
+}
